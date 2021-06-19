a426f0d1789938dda9a844b3798a74b8a486e264
magamy <Newpass@4>
2021-06-20 01:33:09 +0200

    api changed for testing purpose.

--- app/src/main/java/com/example/tempo/data/api/ApiService.kt
+++ app/src/main/java/com/example/tempo/data/api/ApiService.kt
@@ -3,10 +3,10 @@ import com.example.tempo.data.model.ApiResult
 import retrofit2.http.*
 
 interface ApiService {
-    @GET("everything?pageSize=20&apiKey=8cfa9fcf90924f42be7b344c917e8da1")
+    @GET("everything?pageSize=20&apiKey=cdb1446ae5014a74a16f0b1573f19517")
     suspend fun getNewsList(@Query("q") searchKey: String,@Query("page") page: Int): ApiResult
 
-    @GET("everything?q=bitcoin&apiKey=cdb1446ae5014a74a16f0b1573f19517")
+    @GET("everything?q=bitcoin&apiKey=8cfa9fcf90924f42be7b344c917e8da1")
     suspend fun searchNewsList(@Query("") searchKey: String): ApiResult
 
 }
